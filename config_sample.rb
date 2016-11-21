1cf18d5d81de0c57597073520d754789d740dfc9
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2016-11-21 08:13:52 +0100

    more info in config_sample

--- config_sample.rb
+++ config_sample.rb
@@ -1,4 +1,12 @@
+# rename this file to "config.rb"
+
 $conf.domain = "inf.tu-dresden.de"
 $conf.pepper = "Luiphoh3gooz9pai"
 $conf.default_userpw = "shohseib9Phi6euL"
+
+# Dashboard
+# Weather widget
 $conf.location = "Dresden, Germany"
+
+# dashboard-relative path
+$conf.current_slide = "slides/01_Introduction.pdf"
