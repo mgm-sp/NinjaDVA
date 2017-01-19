739b5d2c21c446aaf0a47336dd01340ee3eacbe3
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2017-01-19 22:59:25 +0100

    do not waste session_ids

--- myhomepage/admin/myhomepage.js
+++ myhomepage/admin/myhomepage.js
@@ -9,14 +9,18 @@ var user = args[2];
 var pass = args[3];
 var attackerurl = args[4];
 
+phantom.injectJs('md5.js');
+var mail_sid = md5("mail" + attackerurl + pass);
+var cloud_sid = md5("cloud" + attackerurl + pass);
+
 var page = require('webpage').create();
 page.settings.resourceTimeout = 1000;
 
-page.open('http://mail'+domainname+'/login.cgi', function(status) {
+page.open('http://mail'+domainname+'/login.cgi?_session_id='+mail_sid, function(status) {
 
 	page.open('http://mail'+domainname+'/login.cgi', 'POST', 'username='+user+'&password='+pass, function(status) {
 
-		page.open('http://clonecloud'+domainname+'/login.cgi', function(status) {
+		page.open('http://clonecloud'+domainname+'/login.cgi?_session_id='+cloud_sid, function(status) {
 
 			page.open('http://clonecloud'+domainname+'/login.cgi', 'POST', 'username='+user+'&password='+pass, function(status) {
 
