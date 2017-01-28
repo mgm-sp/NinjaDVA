00e77d469fbb6076309645d6cb1688f53d38969b
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2017-01-28 21:57:07 +0100

    change current to default slide

--- clonecloud/view.cgi
+++ clonecloud/view.cgi
@@ -9,7 +9,7 @@ require 'cgi/session'
 $cgi = CGI.new
 $session = CGI::Session.new($cgi)
 
-h = HTML.new("Current Lecture")
+h = HTML.new("View File")
 h.add_head_script("jquery-2.2.3.min.js")
 
 h.add_html_head(<<STYLE
@@ -20,28 +20,30 @@ height: 95%
 body {
 	height: 100%;
 }
-#current_slide object {
+#file object {
 	width:100%;
 	height:100%;
 }
 </style>
 STYLE
 )
+require "mimemagic"
+file = "data=\"files/#{CGI.escape($cgi["default"])}\" type=#{MimeMagic.by_magic(File.open("files/#{$cgi["default"]}")).type}"
 h << <<CONTENT
-<div id='current_slide' style='text-align: center;height: 100%'>
-<object type="application/pdf" data="#{$conf.current_slide}" > </object>
+<div id='file' style='text-align: center;height: 100%'>
+<object type="application/pdf" #{file} > </object>
 </div>
 CONTENT
 
 h.add_script <<LISTENER
 function chooseFile(event){
 	var file = event.data;
-	$("#current_slide").text("");
-	$("#current_slide").append($("<object data='files/"+file+"'/>",{
+	$("#file").text("");
+	$("#file").append($("<object data='files/"+file+"'/>",{
 		"type" : "application/pdf",
 	}));
 }
-window.addEventListener("message", chooseFile);
+//window.addEventListener("message", chooseFile);
 LISTENER
 
 
--- config_sample.rb
+++ config_sample.rb
@@ -8,8 +8,7 @@ $conf.default_userpw = "shohseib9Phi6euL"
 # Weather widget
 $conf.location = "Dresden, Germany"
 
-# dashboard-relative path
-$conf.current_slide = "slides/01_Introduction.pdf"
+$conf.default_slide = "01_Introduction.pdf"
 
 # time schedule
 $conf.events = [
@@ -22,36 +21,36 @@ $conf.events = [
 		:title => "Session 1",
 		:start => "2016-11-18T09:00:00",
 		:end   => "2016-11-18T11:10:00",
-		:url => 'javascript:chooseMaterial("01-basics-handout.pdf")'
+		:file  => "01-basics-handout.pdf"
 	},
 	{
-		:title => "Bäcker",
-		:start => "2016-11-18T11:15:00",
-		:end   => "2016-11-18T11:30:00",
+		:title           => "Bäcker",
+		:start           => "2016-11-18T11:15:00",
+		:end             => "2016-11-18T11:30:00",
 		:backgroundColor => "#008006"
 	},
 	{
 		:title => "Session 2",
 		:start => "2016-11-18T11:30:00",
 		:end   => "2016-11-18T13:15:00",
-		:url => 'javascript:chooseMaterial("02-security_management-handout.pdf")'
+		:url   => "02-security_management-handout.pdf"
 	},
 	{
-		:title => "Mittagessen",
-		:start => "2016-11-18T13:15:00",
-		:end   => "2016-11-18T14:00:00",
+		:title           => "Mittagessen",
+		:start           => "2016-11-18T13:15:00",
+		:end             => "2016-11-18T14:00:00",
 		:backgroundColor => "#008006"
 	},
 	{
 		:title => "Session 3",
 		:start => "2016-11-18T14:00:00",
 		:end   => "2016-11-18T15:30:00",
-		:url => 'javascript:chooseMaterial("03-Bedrohungen-handout.pdf")'
+		:url   => "03-Bedrohungen-handout.pdf"
 	},
 	{
 		:title => "Session 4",
 		:start => "2016-11-18T15:45:00",
 		:end   => "2016-11-18T17:15:00",
-		:url => 'javascript:chooseMaterial("04-Maßnahmen-handout.pdf")'
+		:url   => "04-Maßnahmen-handout.pdf"
 	}
 ]
