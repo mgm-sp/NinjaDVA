9aab4506b3ab0b68c91852731f42dc2588877003
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2017-02-28 19:01:20 +0100

    use attackers IP in solutions

--- solved.rb
+++ solved.rb
@@ -2,7 +2,7 @@ require "csv"
 class Solution
 	def initialize(category,state,comment="")
 		File.open($conf.solutiondb,"a"){|f|
-			f << [category,ENV['REMOTE_ADDR'],state,comment,Time.now].to_csv
+			f << [category,$cgi.remote_addr,state,comment,Time.now].to_csv
 		}
 	end
 end
