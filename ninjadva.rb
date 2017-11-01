c7b0fc7a160901cbd1231f0058ca4394547c68cb
Robin Herrmann <robin.herrmann@mgm-sp.com>
2017-11-01 14:09:04 +0100

    adapted ninjadva.rb to run in context of the specific vm folder instead of the root folder

--- ninjadva.rb
+++ ninjadva.rb
@@ -1,25 +1,25 @@
 class NinjaDVA
 	def initialize(config, options = {})
 	  # set necessary variables
-	   dashboard_dir = "./dashboard_vm/"
+	   dashboard_dir = "../dashboard_vm/"
 		if !options.key?(:challenge_descriptions_dir)
 			options[:challenge_descriptions_dir] = "challenge-descriptions/"
 		end
 
-		# check whether we are controlled by ninjaDVA Vagrant Manager
-		if Dir.exists?("./dashboard_vm/")
+		# check whether dashboard_vm exists
+		if Dir.exists?(dashboard_dir)
 		  require 'fileutils'
 
 		  # copy tasks to dashboard, before the vm is started
 		  config.trigger.before :up do
-			  yaml_list = Dir.glob("./" + $current_vm_path + "/"+ options[:challenge_descriptions_dir] + "/*.yaml")
+			  yaml_list = Dir.glob("./" + options[:challenge_descriptions_dir] + "/*.yaml")
 			  FileUtils.cp(yaml_list, dashboard_dir + "challenge-descriptions/")
 		 end
 
 		# delete all challanges  included in this vm from the dashboard vm
 	     config.trigger.after :halt do
 			puts "Deleting challenges:"
-			Dir.glob("./" + $current_vm_path + "/" + options[:challenge_descriptions_dir] +"/*.yaml").each{|file|
+			Dir.glob("./" + options[:challenge_descriptions_dir] + "/*.yaml").each{|file|
 			  file_name = File.basename(file)
 			  puts " - " + file_name
 			  path_to_file = dashboard_dir + "challenge-descriptions/" + file_name
