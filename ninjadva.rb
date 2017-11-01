9e651bbbe303ebf0c505975e6be298c74bd24566
Robin Herrmann <robin.herrmann@mgm-sp.com>
2017-11-01 11:21:45 +0100

    initial commit

---
+++ ninjadva.rb
@@ -0,0 +1,31 @@
+class NinjaDVA
+	def initialize(config, options = {})
+	  # set necessary variables
+	   dashboard_dir = "./dashboard_vm/"
+		if !options.key?(:challenge_descriptions_dir)
+			options[:challenge_descriptions_dir] = "challenge-descriptions/"
+		end
+
+		# check whether we are controlled by ninjaDVA Vagrant Manager
+		if Dir.exists?("./dashboard_vm/")
+		  require 'fileutils'
+
+		  # copy tasks to dashboard, before the vm is started
+		  config.trigger.before :up do
+			  yaml_list = Dir.glob("./" + $current_vm_path + "/"+ options[:challenge_descriptions_dir] + "/*.yaml")
+			  FileUtils.cp(yaml_list, dashboard_dir + "challenge-descriptions/")
+		 end
+
+		# delete all challanges  included in this vm from the dashboard vm
+	     config.trigger.after :halt do
+			puts "Deleting challenges:"
+			Dir.glob("./" + $current_vm_path + "/" + options[:challenge_descriptions_dir] +"/*.yaml").each{|file|
+			  file_name = File.basename(file)
+			  puts " - " + file_name
+			  path_to_file = dashboard_dir + "challenge-descriptions/" + file_name
+			  FileUtils.rm(path_to_file) if File.exist?(path_to_file)
+			}
+		 end
+		end
+	end
+end
