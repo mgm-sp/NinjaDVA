b4c06dcad2546377900f46a471e8d10ce72655d9
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2017-11-10 22:14:44 +0100

    indentiation + simple corrections

--- ninjadva.rb
+++ ninjadva.rb
@@ -3,51 +3,51 @@
 # if you start a vm importing the first time and get an error
 class NinjaDVA
 	def initialize(config, options = {})
-	    # add a interface to the vm that is in the same internal network like the gateway vm
-	    config.vm.network "private_network", type: "dhcp", virtualbox__intnet: "ninjadva"
+		# add a interface to the vm that is in the same internal network like the gateway vm
+		config.vm.network "private_network", type: "dhcp", virtualbox__intnet: "ninjadva"
 
-	    # install ninjadva specific software
-	    config.vm.provision "shell", inline: <<~END
-		apt-get -y update
-		apt-get -y install ruby ruby-dev
-		gem install slop
-	    END
-	    config.vm.provision "file", source: "../ninjadva_service", destination: "/home/vagrant/tmp_provision/ninjadva_service"
-	    config.vm.provision "shell", inline: <<~END
-		cd /home/vagrant/tmp_provision/
-		cp -R ninjadva_service /usr/share/
-		chmod +x /usr/share/ninjadva_service/ninjasolver.rb
-		ln -sf /usr/share/ninjadva_service/ninjasolver.rb /usr/local/bin/ninjasolver
-		rm -rf *
-	    END
+		# install ninjadva specific software
+		config.vm.provision "shell", inline: <<~END
+			apt-get -y update
+			apt-get -y install ruby ruby-dev
+			gem install slop
+		END
+		config.vm.provision "file", source: "../ninjadva_service", destination: "/home/vagrant/tmp_provision/ninjadva_service"
+		config.vm.provision "shell", inline: <<~END
+			cd /home/vagrant/tmp_provision/
+			cp -R ninjadva_service /usr/share/
+			chmod +x /usr/share/ninjadva_service/ninjasolver.rb
+			ln -sf /usr/share/ninjadva_service/ninjasolver.rb /usr/local/bin/ninjasolver
+			rm -rf *
+		END
 
-		
-	  # set necessary variables for provision of challenges
-	   dashboard_dir = "../dashboard_vm/"
+
+		# set necessary variables for provision of challenges
+		dashboard_dir = "../dashboard_vm/"
 		if !options.key?(:challenge_descriptions_dir)
 			options[:challenge_descriptions_dir] = "challenge-descriptions/"
 		end
 
 		# check whether dashboard_vm exists
 		if Dir.exists?(dashboard_dir)
-		  require 'fileutils'
+			require 'fileutils'
 
-		  # copy challenges to dashboard, before the vm is started
-		  config.trigger.before :up do
-			  yaml_list = Dir.glob("./" + options[:challenge_descriptions_dir] + "/*.yaml")
-			  FileUtils.cp(yaml_list, dashboard_dir + "challenge-descriptions/")
-		 end
+			# copy challenges to dashboard, before the vm is started
+			config.trigger.before :up do
+				yaml_list = Dir.glob("./" + options[:challenge_descriptions_dir] + "/*.yaml")
+				FileUtils.cp(yaml_list, dashboard_dir + "challenge-descriptions/")
+			end
 
-		# delete all challenges  included in this vm from the dashboard vm
-	     config.trigger.after :halt do
-			puts "Deleting challenges:"
-			Dir.glob("./" + options[:challenge_descriptions_dir] + "/*.yaml").each{|file|
-			  file_name = File.basename(file)
-			  puts " - " + file_name
-			  path_to_file = dashboard_dir + "challenge-descriptions/" + file_name
-			  FileUtils.rm(path_to_file) if File.exist?(path_to_file)
-			}
-		 end
+			# delete all challenges  included in this vm from the dashboard vm
+			config.trigger.after :halt do
+				puts "Deleting challenges:"
+				Dir.glob("./" + options[:challenge_descriptions_dir] + "/*.yaml").each{|file|
+					file_name = File.basename(file)
+					puts " - " + file_name
+					path_to_file = dashboard_dir + "challenge-descriptions/" + file_name
+					FileUtils.rm(path_to_file) if File.exist?(path_to_file)
+				}
+			end
 		end
 	end
 end
--- ninjadva_service/ninjasolver.rb
+++ ninjadva_service/ninjasolver.rb
@@ -10,32 +10,32 @@ require 'uri'
 require 'slop'
 
 begin
-opts = Slop.parse() do |o|
-    o.string '-r', '--remote_solution_handler_url', '(required) url of the solution handler script on the dashboard vm', required: true
-    o.string '-i', '--ip_addr', '(required) ip address of the participant solved the challenge', required: true
-    o.string '-c', '--category', '(required) category of the solution', required: true
-    o.integer '-s', '--state', '(required) state of the challenge', required: true
-    o.string '--comment', '(optional) comment for the trainer'
-    o.on '--help', 'show this help message' do
-        puts o
-        exit
-      end
-end
+	opts = Slop.parse() do |o|
+		o.string '-r', '--remote_solution_handler_url', '(required) url of the solution handler script on the dashboard vm', required: true
+		o.string '-i', '--ip_addr', '(required) ip address of the participant solved the challenge', required: true
+		o.string '-c', '--category', '(required) category of the solution', required: true
+		o.integer '-s', '--state', '(required) state of the challenge', required: true
+		o.string '--comment', '(optional) comment for the trainer'
+		o.on '--help', 'show this help message' do
+			puts o
+			exit
+		end
+	end
 rescue
- puts "Arguments Error: use the --help flag to get more information about the right usage"
- exit 1
+	puts "Arguments Error: use the --help flag to get more information about the right usage"
+	exit 1
 end
 
 #puts opts
 
 def send_solve(remote_solution_handler_url, ip_addr, category, state, comment="")
-        post_payload = {
-            'ip_addr' => ip_addr,
-            'category' => category,
-            'state' => state,
-            'comment' => comment ? comment : ""
-        }
-        Net::HTTP.post_form(URI.parse(remote_solution_handler_url), post_payload)
+	post_payload = {
+		'ip_addr' => ip_addr,
+		'category' => category,
+		'state' => state,
+		'comment' => comment ? comment : ""
+	}
+	Net::HTTP.post_form(URI.parse(remote_solution_handler_url), post_payload)
 end
 
 send_solve(opts[:remote_solution_handler_url], opts[:ip_addr], opts[:category], opts[:state], opts[:comment])
