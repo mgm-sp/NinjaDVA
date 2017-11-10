b6b2a2f34015a522e6fe10e1ca41fa19747956dd
Robin Herrmann <robin.herrmann@mgm-sp.com>
2017-11-10 09:13:42 +0100

    moved ninjadva specific provision to ninjadva.rb and restyled README

--- ninjadva.rb
+++ ninjadva.rb
@@ -1,6 +1,28 @@
+# Requirements:
+# Please execute `vagrant plugin install vagrant-triggers`
+# if you start a vm importing the first time and get an error
 class NinjaDVA
 	def initialize(config, options = {})
-	  # set necessary variables
+	    # add a interface to the vm that is in the same internal network like the gateway vm
+	    config.vm.network "private_network", type: "dhcp", virtualbox__intnet: "ninjadva"
+
+	    # install ninjadva specific software
+	    config.vm.provision "shell", inline: <<~END
+		apt-get -y update
+		apt-get -y install ruby ruby-dev
+		gem install slop
+	    END
+	    config.vm.provision "file", source: "../ninjadva_service", destination: "/home/vagrant/tmp_provision/ninjadva_service"
+	    config.vm.provision "shell", inline: <<~END
+		cd /home/vagrant/tmp_provision/
+		cp -R ninjadva_service /usr/share/
+		chmod +x /usr/share/ninjadva_service/ninjasolver.rb
+		ln -sf /usr/share/ninjadva_service/ninjasolver.rb /usr/local/bin/ninjasolver
+		rm -rf *
+	    END
+
+		
+	  # set necessary variables for provision of challenges
 	   dashboard_dir = "../dashboard_vm/"
 		if !options.key?(:challenge_descriptions_dir)
 			options[:challenge_descriptions_dir] = "challenge-descriptions/"
@@ -10,13 +32,13 @@ class NinjaDVA
 		if Dir.exists?(dashboard_dir)
 		  require 'fileutils'
 
-		  # copy tasks to dashboard, before the vm is started
+		  # copy challenges to dashboard, before the vm is started
 		  config.trigger.before :up do
 			  yaml_list = Dir.glob("./" + options[:challenge_descriptions_dir] + "/*.yaml")
 			  FileUtils.cp(yaml_list, dashboard_dir + "challenge-descriptions/")
 		 end
 
-		# delete all challanges  included in this vm from the dashboard vm
+		# delete all challenges  included in this vm from the dashboard vm
 	     config.trigger.after :halt do
 			puts "Deleting challenges:"
 			Dir.glob("./" + options[:challenge_descriptions_dir] + "/*.yaml").each{|file|
--- your_vulnerable_vm/README.md
+++ your_vulnerable_vm/README.md
@@ -7,12 +7,17 @@ Instructions to add your Vulnerable VM to NinjaDVA
 
 Submit Solutions
 ================
-To inform the dashboard that a participant solved a challenge, use the installed program ninjasolver [options]
+To inform the dashboard that a participant solved a challenge, use the installed program 
+```
+ninjasolver [options]
 -r, --remote_solution_handler_url  (required) url of the solution handler script on the dashboard vm
 -i, --ip_addr                      (required) ip address of the participant solved the challenge
 -c, --category                     (required) category of the solution
 -s, --state                        (required) state of the challenge
 --comment                          (optional) comment for the trainer
 
-Example: ninjasolver -r http://dashboard.mgmsp-lab.com/solve_srv.cgi -i 172.23.42.1 -c your_vm_xss1
-											-s 10 --comment "He read the mails of Mr. Schmidt"
+Example: 
+
+ninjasolver -r http://dashboard.mgmsp-lab.com/solve_srv.cgi -i 172.23.42.1 -c your_vm_xss1
+    -s 10 --comment "He read the mails of Mr. Schmidt"
+```
