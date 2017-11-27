0931605717da5b29f1b82b3127f7b3d7961361cf
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2017-11-27 09:50:33 +0100

    bufgix: fallback to server grid layout if no local layout given

--- dashboard_vm/www/dashboard/admin.js
+++ dashboard_vm/www/dashboard/admin.js
@@ -3,13 +3,13 @@ var showAdminInterface = function () {
 	toggleAdminInterface = hideAdminInterface;
 	$.ajax("/admin/widgets.cgi").success(function(html){
 		$(".gridster").append(html);
-		grid_layout_from_localstorage();
+		grid_layout_from_server(true);
 	});
 }
 var hideAdminInterface = function () {
 	grid_store_suffix = "user";
 	toggleAdminInterface = showAdminInterface;
 	gridster.remove_widget($(".adminwidget"));
-	grid_layout_from_localstorage();
+	grid_layout_from_server(true);
 }
 var toggleAdminInterface = showAdminInterface;
