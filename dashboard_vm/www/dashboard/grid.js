aeb1d9949be55e6b5f19b3e2f8f84ae1703f8740
Robin Herrmann <robin.herrmann@mgm-sp.com>
2017-12-20 12:25:12 +0100

    changed option in grid.js

--- dashboard_vm/www/dashboard/grid.js
+++ dashboard_vm/www/dashboard/grid.js
@@ -1,111 +1,119 @@
 var gridster;
-$(function(){ //DOM Ready
-	gridster = $("div.gridster").gridster({
-			widget_base_dimensions: [170, 100],
-			widget_margins: [12, 12],
-			widget_selector: "div.widget",
-			draggable: {
-				handle: 'h1',
-				start: function(){
-					gridster.empty_cells_player_occupies ();
-				},
-				stop: function () {
-					$(".gridnav").show();
-				}
-			},
-			shift_widgets_up: false,
-			shift_larger_widgets_down: true,
-			collision: {
-				wait_for_mouseup: true
-			},
-			resize: {
-				enabled: true,
-				stop: function(){
-					adjust_calendar();
-					$(".gridnav").show();
-				},
-				resize: adjust_calendar
-			}
-	}).data('gridster').enable();
-	grid_layout_from_server(true);
+$(function() {
+    //DOM Ready
+    gridster = $("div.gridster")
+        .gridster({
+            widget_base_dimensions: [170, 100],
+            widget_margins: [12, 12],
+            widget_selector: "div.widget",
+            avoid_overlapped_widgets: true,
+            draggable: {
+                handle: "h1",
+                start: function() {
+                    gridster.empty_cells_player_occupies();
+                },
+                stop: function() {
+                    $(".gridnav").show();
+                }
+            },
+            shift_widgets_up: false,
+            shift_larger_widgets_down: true,
+            collision: {
+                wait_for_mouseup: true
+            },
+            resize: {
+                enabled: true,
+                stop: function() {
+                    adjust_calendar();
+                    $(".gridnav").show();
+                },
+                resize: adjust_calendar
+            }
+        })
+        .data("gridster")
+        .enable();
+    grid_layout_from_server(true);
 });
 
-function adjust_calendar(){
-	$('#calendar').fullCalendar('option','height',$('#calendarwidget').height()-53);
+function adjust_calendar() {
+    $("#calendar").fullCalendar("option", "height", $("#calendarwidget").height() - 53);
 }
 
-function grid_layout_from_server(load_layout_from_localstorage){
-	$.ajax({
-		url: "/grid_layout.cgi",
-		type: "get",
-		success: function(gridlayout){
-			set_grid_layout(gridlayout);
-			if (load_layout_from_localstorage) {
-				grid_layout_from_localstorage();
-			}
-			$(".gridnav").hide();
-		}
-	});
+function grid_layout_from_server(load_layout_from_localstorage) {
+    $.ajax({
+        url: "/grid_layout.cgi",
+        type: "get",
+        success: function(gridlayout) {
+            set_grid_layout(gridlayout);
+            if (load_layout_from_localstorage) {
+                grid_layout_from_localstorage();
+            }
+            $(".gridnav").hide();
+        }
+    });
 }
 
-function get_current_grid_layout(){
-	var layout = {};
-	$(".widget").each(function(i,u){
-
-		layout[u.id] = {
-			"col" : $(u).attr("data-col"),
-			"row" : $(u).attr("data-row"),
-			"sizex" : $(u).attr("data-sizex"),
-			"sizey" : $(u).attr("data-sizey"),
-		};
-	});
-	return(layout);
+function get_current_grid_layout() {
+    var layout = {};
+    $(".widget").each(function(i, u) {
+        layout[u.id] = {
+            col: $(u).attr("data-col"),
+            row: $(u).attr("data-row"),
+            sizex: $(u).attr("data-sizex"),
+            sizey: $(u).attr("data-sizey")
+        };
+    });
+    return layout;
 }
 
 // this global variable should be in sync with the current state
 // if user or admin view is shown
 var grid_store_suffix = "user";
-function save_grid_layout_to_localstorage(){
-	window.localStorage.setItem('gridster-'+document.location.pathname+grid_store_suffix,JSON.stringify(get_current_grid_layout()));
-	$(".gridnav").hide();
+function save_grid_layout_to_localstorage() {
+    window.localStorage.setItem(
+        "gridster-" + document.location.pathname + grid_store_suffix,
+        JSON.stringify(get_current_grid_layout())
+    );
+    $(".gridnav").hide();
 }
 
-function revert_grid_layout_to_server(){
-	window.localStorage.removeItem('gridster-'+document.location.pathname);
-	$(".gridnav").hide();
-	grid_layout_from_server(true);
+function revert_grid_layout_to_server() {
+    window.localStorage.removeItem("gridster-" + document.location.pathname);
+    $(".gridnav").hide();
+    grid_layout_from_server(true);
 }
 
-function grid_layout_from_localstorage(){
-	gridlayout = JSON.parse(window.localStorage.getItem('gridster-'+document.location.pathname+grid_store_suffix));
-	if (gridlayout) {
-		set_grid_layout(gridlayout);
-	}
-	$(".gridnav").hide();
+function grid_layout_from_localstorage() {
+    gridlayout = JSON.parse(window.localStorage.getItem("gridster-" + document.location.pathname + grid_store_suffix));
+    if (gridlayout) {
+        set_grid_layout(gridlayout);
+    }
+    $(".gridnav").hide();
 }
 
-function set_grid_layout(gridlayout){
-	$(".widget").each(function(i,u){
-		layout = gridlayout[u.id];
-		if (layout){
-			gridster.move_widget($("#"+u.id), parseInt(layout["col"]), parseInt(layout["row"]));
-			gridster.resize_widget($("#"+u.id), parseInt(layout["sizex"]), parseInt(layout["sizey"]));
-		}
-	});
-	window.setTimeout(adjust_calendar,500);
+function set_grid_layout(gridlayout) {
+    $(".widget").each(function(i, u) {
+        layout = gridlayout[u.id];
+        if (layout) {
+            gridster.move_widget($("#" + u.id), parseInt(layout["col"]), parseInt(layout["row"]));
+            gridster.resize_widget($("#" + u.id), parseInt(layout["sizex"]), parseInt(layout["sizey"]));
+        }
+    });
+    window.setTimeout(adjust_calendar, 500);
 }
-
 
 // zoom button for all widgets
-$(function(){ //DOM Ready
-	var zoomButton = "<img class='zoomButton' style='cursor:pointer;height:2ex;width:2ex' src='expand-256.png' onclick='toggleMaximize()'>";
-	$("div.widget h1").append(zoomButton);
+$(function() {
+    //DOM Ready
+    var zoomButton =
+        "<img class='zoomButton' style='cursor:pointer;height:2ex;width:2ex' src='expand-256.png' onclick='toggleMaximize()'>";
+    $("div.widget h1").append(zoomButton);
 });
 
-function toggleMaximize(){
-	var e = "expand-256.png";
-	var c = "collapse-256.png";
-	var i = $(".zoomButton");
-	i.attr("src", i.attr("src") === e ? c : e);
-	$("div.widget:hover").toggleClass("fullscreen");
+function toggleMaximize() {
+    var e = "expand-256.png";
+    var c = "collapse-256.png";
+    var i = $(".zoomButton");
+    i.attr("src", i.attr("src") === e ? c : e);
+    $("div.widget:hover").toggleClass("fullscreen");
 }
