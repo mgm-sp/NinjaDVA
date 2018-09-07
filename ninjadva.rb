3b6bb734f62a3355c64d60e8a720510c3c66b15d
Benjamin Kellermann <Benjamin.Kellermann@mgm-sp.com>
2018-09-07 10:47:35 +0200

    adopt to Vagrant 2.1 (new trigger implementation)

--- ninjadva.rb
+++ ninjadva.rb
@@ -1,18 +1,43 @@
-# Requirements:
-# Please execute `vagrant plugin install vagrant-triggers`
-# if you start a vm importing the first time and get an error
+require "yaml"
+
 class NinjaDVA
+	def verbose_copy(comment,from,to)
+		file_ary = Dir.glob(from)
+		FileUtils.cp(file_ary, to)
+		verbose_print(comment,file_ary) if @options[:verbose]
+	end
+	def verbose_delete(comment,files,target_dir)# deletes basename of files(wildcard) in target_dir
+		file_ary = Dir.glob(files)
+		verbose_print(comment,file_ary) if @options[:verbose]
+		file_ary.each{|file|
+			file_name = File.basename(file)
+			path_to_file = target_dir + file_name
+			FileUtils.rm(path_to_file) if File.exist?(path_to_file)
+		}
+	end
+	def verbose_print(headline,file_array)
+		puts headline
+		file_array.each{|c|
+			puts " - #{File.basename(c)}"
+		}
+	end
 	def initialize(config, options = {})
+		@options = options
 		# set some defaults
 		options[:ninjadva_dir] ||= ".."
 		options[:challenge_descriptions_dir] ||= "./ninjadva/challenge-descriptions/"
 		options[:dashboard_widgets_dir] ||= "./ninjadva/dashboard-widgets/"
 		options[:dashboard_admin_widgets_dir] ||= "./ninjadva/dashboard-admin/"
-		options[:link_widget_links] ||= [{ hostname: config.vm.hostname, name: config.vm.hostname }]
+		if config.vm.hostname.class == String
+			options[:link_widget_links] ||= [{ hostname: config.vm.hostname, name: config.vm.hostname }]
+		else
+			options[:link_widget_links] ||= []
+		end
 
 		if File.exists?("#{options[:ninjadva_dir]}/.ninjadvarc.yaml")
 			ninjadvarc = YAML::load_file("#{options[:ninjadva_dir]}/.ninjadvarc.yaml")
 			options[:ssh_key] ||= ninjadvarc["ssh_key"] if ninjadvarc["ssh_key"]
+			options[:verbose] ||= ninjadvarc["verbose"] if ninjadvarc["verbose"]
 		end
 
 		if options[:ssh_key]
@@ -48,76 +73,64 @@ class NinjaDVA
 		END
 
 
-		# set necessary variables for provision of challenges
-		dashboard_dir = "#{options[:ninjadva_dir]}/dashboard_vm/"
-
-		# check whether dashboard_vm exists
-		if Dir.exists?(dashboard_dir)
-			require 'fileutils'
-
-			# copy challenges and widgets to dashboard, before the vm is started
-			config.trigger.before :up do
-				chal_list = Dir.glob(options[:challenge_descriptions_dir] + "/*.yaml")
-				FileUtils.cp(chal_list, dashboard_dir + "challenge-descriptions/")
-
-				widget_list = Dir.glob(options[:dashboard_widgets_dir] + "/*.html")
-				FileUtils.cp(widget_list, dashboard_dir + "dashboard-widgets/")
-
-				admin_widget_list = Dir.glob(options[:dashboard_admin_widgets_dir] + "/*.html")
-				FileUtils.cp(admin_widget_list, dashboard_dir + "dashboard-admin/")
-
-				# add host to list of available favourite links
-				linkfile = "#{dashboard_dir}/dashboard-widgets/available_favourite_links.yaml"
-				require "yaml"
-				if File.exists?(linkfile)
-					currentlinks = YAML::load_file(linkfile)
-				else
-					currentlinks = []
-				end
-				currentlinks += options[:link_widget_links]
-				currentlinks.uniq!
-				File.open(linkfile,"w") {|f| f << currentlinks.to_yaml }
-			end
-
-			# delete all challenges/widgets included in this vm from the dashboard vm
-			[:halt,:destroy].each do |trigger|
-				config.trigger.after trigger do
-					puts "Deleting challenges:"
-					Dir.glob(options[:challenge_descriptions_dir] + "/*.yaml").each{|file|
-						file_name = File.basename(file)
-						puts " - " + file_name
-						path_to_file = dashboard_dir + "challenge-descriptions/" + file_name
-						FileUtils.rm(path_to_file) if File.exist?(path_to_file)
-					}
-					puts "Deleting widgets:"
-					Dir.glob(options[:dashboard_widgets_dir] + "/*.html").each{|file|
-						file_name = File.basename(file)
-						puts " - " + file_name
-						path_to_file = dashboard_dir + "dashboard-widgets/" + file_name
-						FileUtils.rm(path_to_file) if File.exist?(path_to_file)
-					}
-
-					Dir.glob(options[:dashboard_admin_widgets_dir] + "/*.html").each{|file|
-						file_name = File.basename(file)
-						puts " - " + file_name
-						path_to_file = dashboard_dir + "dashboard-admin/" + file_name
-						FileUtils.rm(path_to_file) if File.exist?(path_to_file)
-					}
-
-					# delete host from list of available favourite links
-					linkfile = "#{dashboard_dir}/dashboard-widgets/available_favourite_links.yaml"
-					require "yaml"
-					if File.exists?(linkfile)
-						currentlinks = YAML::load_file(linkfile)
-					else
-						currentlinks = []
-					end
-					options[:link_widget_links].each{|link|
-						currentlinks.delete(link)
-					}
-					File.open(linkfile,"w") {|f| f << currentlinks.to_yaml }
-				end
-			end
+		# copy challenges and widgets to dashboard, before the vm is started
+		config.trigger.after :up do |trigger|
+			trigger.info = "Register VM on Dashboard"
+			trigger.run = {
+				path: "#{options[:ninjadva_dir]}/ninjadva",
+				args: [
+					"register",
+					File.basename(Dir.pwd),
+					options[:challenge_descriptions_dir],
+					options[:dashboard_widgets_dir],
+					options[:dashboard_admin_widgets_dir]
+				]
+			}
 		end
+
+		## add host to list of available favourite links
+		options[:link_widget_links].each{|link|
+			config.trigger.after :up do |trigger|
+				trigger.info = "Register Link: #{link[:hostname]}"
+				trigger.run = {
+					path: "#{options[:ninjadva_dir]}/ninjadva",
+					args: [
+						"addlink",
+						link[:hostname],
+						link[:name]
+					]
+				}
+			end
+		}
+
+		# delete all challenges/widgets included in this vm from the dashboard vm
+		config.trigger.before [:halt,:destroy] do |trigger|
+			trigger.info = "Deregister VM on Dashboard"
+			trigger.run = {
+				path: "#{options[:ninjadva_dir]}/ninjadva",
+				args: [
+					"deregister",
+					File.basename(Dir.pwd),
+					options[:challenge_descriptions_dir],
+					options[:dashboard_widgets_dir],
+					options[:dashboard_admin_widgets_dir]
+				]
+			}
+		end
+
+		## delete host from list of available favourite links
+		options[:link_widget_links].each{|link|
+			config.trigger.before [:halt,:destroy] do |trigger|
+				trigger.info = "Delete Link: #{link[:hostname]}"
+				trigger.run = {
+					path: "#{options[:ninjadva_dir]}/ninjadva",
+					args: [
+						"rmlink",
+						link[:hostname],
+						link[:name]
+					]
+				}
+			end
+		}
 	end
 end
